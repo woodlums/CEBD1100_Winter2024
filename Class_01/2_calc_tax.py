aaf0865a294d13a80c9df737a6ddbcc5d9d0f0c8
Brendan Wood <brendan.wood@concordia.ca>
2024-01-24 19:41:15 -0500

    Improved exercise 2 week 1

--- Class_01/2_calc_tax.py
+++ Class_01/2_calc_tax.py
@@ -1,10 +1,11 @@
-TAX_RATE = 1.14975
+# Example of how to use constants instead of hard-coding numbers.
 
+TAX_RATE = 1.14975
 price = 59.99
 
-total = price * TAX_RATE
+grand_total = price * TAX_RATE
 
 # To convert to string, use function --->  str(....)
 # to convert to a float, use function ---> float(....)
 
-print("The price with tax is " + str(total))
+print("The price with tax is " + str(grand_total))
