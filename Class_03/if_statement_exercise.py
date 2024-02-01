1e6acd6f4999d283d208c28ca3bb6a33f2047f85
Brendan Wood <brendan.wood@concordia.ca>
2024-01-31 20:46:54 -0500

    End of class 3

--- Class_03/if_statement_exercise.py
+++ Class_03/if_statement_exercise.py
@@ -2,21 +2,27 @@ discount = 0.00
 RETAIL_CUST = "R"
 WHOLESALE_CUST = "W"
 
-customer_type = input("What is the customer type? :")
-invoice_amount = float(input("What is the invoice amount?"))
+while True:
+    customer_type = input("What is the customer type? :")
+    if customer_type.upper() == RETAIL_CUST or customer_type.upper() == WHOLESALE_CUST:
+        break
+    print("Sorry - please enter the correct value (R or W) - try again")
+
+invoice_amount = float(input("What is the invoice amount?")) # Accept a number
+
+
 
 if customer_type.upper() == RETAIL_CUST:
     if invoice_amount >= 100.00 and invoice_amount < 250:
         discount = .1
     if invoice_amount >= 250:
         discount = .2
-elif customer_type.upper() == WHOLESALE_CUST:
+if customer_type.upper() == WHOLESALE_CUST:
     if invoice_amount < 500:
         discount = .4
     if invoice_amount >= 500:
         discount = .2
-else:
-    print("Sorry the customer type is incorrect.")
+
 
 print("The discount determined is : {:.0%}".format(discount))
 
