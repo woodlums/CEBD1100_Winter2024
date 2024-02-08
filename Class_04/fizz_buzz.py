57aa9326231e97b1e9d7ef231414b09deaa8b2b8
Brendan Wood <brendan.wood@concordia.ca>
2024-02-07 21:34:44 -0500

    End of class 4 FizzBuzz improved

--- Class_04/fizz_buzz.py
+++ Class_04/fizz_buzz.py
@@ -1,7 +1,15 @@
  # FizzBuzz
 
-rules = [(3, "Fizz"), (5, "Buzz"), (7, "Pop")]
+#rules = [(3, "Fizz"), (5, "Buzz"), (7, "Pop")]
 
+rules = []
+ans = ""
+
+while ans.upper() != "X":
+    rule = [int(input("Enter the number to match (multiple of x):")), input("What do I print for that multiple?:")]
+    ans = input("Press enter for next, or enter 'X' when finished.")
+    rules.append(rule)
+print()
 for x in range(26):
     print(str(x) + " : ", end="")
     for rule in rules:
