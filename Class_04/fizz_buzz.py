b0b95667d7d28115a88de1e24b15a9916bb4ced7
Brendan Wood <brendan.wood@concordia.ca>
2024-02-07 21:18:42 -0500

    End of class 4 FizzBuzz

--- Class_04/fizz_buzz.py
+++ Class_04/fizz_buzz.py
@@ -1,18 +1,10 @@
  # FizzBuzz
 
+rules = [(3, "Fizz"), (5, "Buzz"), (7, "Pop")]
+
 for x in range(26):
-    print(str(x) + " :", end="")
-
-    div_3 = x % 3 == 0
-    div_5 = x % 5 == 0
-    div_7 = x % 7 == 0
-
-
-    if div_3:
-        print("Fizz", end="")
-    if div_5:
-        print("Buzz", end="")
-    if div_7:
-        print("Pop", end="")
-
+    print(str(x) + " : ", end="")
+    for rule in rules:
+        if x % rule[0] == 0:
+            print(rule[1], end="")
     print()
